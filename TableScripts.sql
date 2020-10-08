d978d6a3a6c86a5e5458ee8f08a4fd59f2f2bb1a
Ayushi Indani <ayushi.indani@gmail.com>
2020-10-08 11:02:00 +0530

    Add files via upload

---
+++ TableScripts.sql
@@ -0,0 +1,6 @@
+USE [TestDB]
+GO
+INSERT [dbo].[student] ([student_key], [student_code], [student_name]) VALUES (1, N'a', N'Ayushi')
+GO
+INSERT [dbo].[student] ([student_key], [student_code], [student_name]) VALUES (2, N'b', N'Prachi')
+GO
